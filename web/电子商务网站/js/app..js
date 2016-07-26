507814bc8b56b11989a580afe688c6e6d6b01d15
Lt-grint <1598552894@qq.com>
2016-07-26 16:25:09 +0800

    “‘’增加顶部导航条底部黄色色块效果

---
+++ web/电子商务网站/js/app..js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+//  $(".nav a").mouseover(function(){
+//      $("#current_nav").style.backgroundColor:yellow;
+//  });
+//  $(".header .down .nav ul>li a").mouseout(function(){
+//      $("#current_nav").style.backgroundColor:#ffffff;
+//  });
+	var lift_nav_width=$(".left_side_bar").attr("width");
+	console.log(lift_nav_width);
+});
